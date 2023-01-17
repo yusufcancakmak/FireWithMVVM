1cfcb908e049e4854661329667a97686ef79cd04
yusufcan <yusufcakmak29@gmail.com>
2023-01-17 16:36:13 +0300

    This project

---
+++ app/src/main/java/com/yusufcancakmak/firebasewithmvvm/note/NoteListingFragment.kt
@@ -0,0 +1,33 @@
+package com.yusufcancakmak.firebasewithmvvm.note
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yusufcancakmak.firebasewithmvvm.databinding.FragmentNoteListingBinding
+
+class NoteListingFragment : Fragment() {
+    val TAG: String ="Note Listining Fragment"
+    lateinit var binding: FragmentNoteListingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding= FragmentNoteListingBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+    }
